372d6ba3f56601c50d6550f2d8936f37a282c2c8
Ashley Felton <ashley@ropable.com>
2015-04-21 13:41:42 +0800

    Remove unused imports, styling.

--- observations/sites.py
+++ observations/sites.py
@@ -1,39 +1,49 @@
+from arrow import Arrow
+from collections import OrderedDict
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
-User = get_user_model()
-
-#from django.contrib.auth.models import User
-from django.db.models import Avg,Aggregate,Count
 from django.db import models
+from django.db.models import Avg, Count
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
-#from django.contrib.flatpages.admin import FlatPageAdmin
-#from django.contrib.flatpages.models import FlatPage
-from flatpages_x.admin import FlatPageImage,Revision
+from flatpages_x.admin import FlatPageImage, Revision
 from flatpages_x.models import FlatPage
-
-from observations.admin import (SiteAdmin, CameraAdmin, PenguinCountAdmin,
-                                PenguinObservationAdmin, VideoAdmin,HelpCMS)
-from observations.models import (Site, Camera, PenguinCount,
-                                 PenguinObservation, Video,PenguinUser,GraphForm)
-
-
-from arrow import Arrow
 import logging
 
+from observations.admin import (SiteAdmin, CameraAdmin, PenguinCountAdmin,
+                                PenguinObservationAdmin, VideoAdmin, HelpCMS)
+from observations.models import (Site, Camera, PenguinCount,
+                                 PenguinObservation, Video, PenguinUser, GraphForm)
+
+
 logger = logging.getLogger(__name__)
+User = get_user_model()
 
 
 class PenguinUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active','is_superuser','is_staff','last_login')}),
-        (_('Statistics'),  {'fields': ('observation_count','completion_count')}),
+        (_('Permissions'),
+         {'fields': ('is_active',
+                     'is_superuser',
+                     'is_staff',
+                     'last_login')}),
+        (_('Statistics'), {
+         'fields': ('observation_count', 'completion_count')}),
     )
-    list_display = ('username','email','first_name','last_name','is_superuser','is_staff','is_active'
-        ,'observation_count','completion_count','completion_hours')
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+        'observation_count',
+        'completion_count',
+        'completion_hours')
 
     list_filter = ('is_superuser',)
 
@@ -51,19 +61,19 @@ class PenguinUserAdmin(UserAdmin):
         context.update(extra_context or {})
         return super(PenguinUserAdmin, self).changelist_view(request, context)
 
-    readonly_fields = ('last_login','observation_count','completion_count')
+    readonly_fields = ('last_login', 'observation_count', 'completion_count')
 
     def response_add(self, request, obj, post_url_continue=None):
         return super(PenguinUserAdmin, self).response_add(request, obj,
-                                                   post_url_continue)
-    #class Meta:
-    #    model = get_user_model()
+                                                          post_url_continue)
 
 
 class MedianSQL(models.sql.aggregates.Aggregate):
     sql_function = 'median'
 
+
 class Median(models.Aggregate):
+
     """
 
     Migration 0008 adds the following;-
@@ -81,7 +91,7 @@ class Median(models.Aggregate):
        ) sub;
     $$
     LANGUAGE 'sql' IMMUTABLE;
-     
+
     DROP AGGREGATE IF EXISTS median(numeric)
 
     CREATE AGGREGATE median(numeric) (
@@ -92,13 +102,15 @@ class Median(models.Aggregate):
     );
     """
 
-    name='Median'
+    name = 'Median'
+
     def add_to_query(self, query, alias, col, source, is_summary):
         aggregate = MedianSQL(col,
-                                       source=source,
-                                       is_summary=is_summary,
-                                       **self.extra)
-        query.aggregates[alias] = aggregate   
+                              source=source,
+                              is_summary=is_summary,
+                              **self.extra)
+        query.aggregates[alias] = aggregate
+
 
 class PenguinSite(AdminSite):
 
@@ -112,8 +124,6 @@ class PenguinSite(AdminSite):
         today = Arrow.fromdatetime(now())
         last_year = today.replace(months=-11)
 
-        from collections import OrderedDict
-
         site_dataset = OrderedDict()
 
         sites = []
@@ -121,8 +131,10 @@ class PenguinSite(AdminSite):
         if request.user.is_superuser:
             sites = Site.objects.annotate(video_count=Count('camera__video'))
         else:
-            sites = Site.objects.annotate(video_count=Count('camera__video')).filter(id__lte=2).exclude(pk=17)
-        #import ipdb; ipdb.set_trace()
+            sites = Site.objects.annotate(
+                video_count=Count('camera__video')).filter(
+                id__lte=2).exclude(
+                pk=17)
 
         gf = GraphForm(request.GET)
 
@@ -133,13 +145,13 @@ class PenguinSite(AdminSite):
             startd = Arrow.fromdate(gf.cleaned_data['start_date'])
             endd = Arrow.fromdate(gf.cleaned_data['end_date'])
 
-        difference = (endd-startd).days
+        difference = (endd - startd).days
 
         period = 'month'
 
         if difference < 31:
             period = 'day'
-        elif difference < (30*6):
+        elif difference < (30 * 6):
             period = 'week'
         else:
             period = 'month'
@@ -148,41 +160,37 @@ class PenguinSite(AdminSite):
         # across the entire month. These calculations are the average over
         # the median number of penguins observed each day.
 
-        site_dataset['Total Penguins']= []
+        site_dataset['Total Penguins'] = []
         for site in sites:
             site_dataset[site.name] = []
-            for start, end in Arrow.span_range(period,startd,endd):
+            for start, end in Arrow.span_range(period, startd, endd):
                 average = site.penguincount_set.filter(
                     date__gte=start.date(), date__lte=end.date()
                 ).aggregate(penguins=Avg('total_penguins'))
                 site_dataset[site.name].append({
                     'date': start.date(),
-                    'value': "%0.2f" % average['penguins'] if (average['penguins']>0) else 0.0
+                    'value': "%0.2f" % average['penguins'] if (average['penguins'] > 0) else 0.0
                 })
                 for item in site_dataset['Total Penguins']:
                     if item['date'] == start.date():
-                        item['value'] = "%0.2f" % (float(item['value']) + (average['penguins'] if (average['penguins']>0) else 0.0))
-                        break;
+                        item['value'] = "%0.2f" % (float(
+                            item['value']) + (average['penguins'] if (average['penguins'] > 0) else 0.0))
+                        break
                 else:
                     site_dataset['Total Penguins'].append({
                         'date': start.date(),
-                        'value': "%0.2f" % average['penguins'] if (average['penguins']>0) else 0.0
+                        'value': "%0.2f" % average['penguins'] if (average['penguins'] > 0) else 0.0
                     })
-
-
-
-        #import ipdb; ipdb.set_trace()
-
-
 
         context = {
             'sites': sites,
             'site_dataset': site_dataset,
             'title': _("Penguin island sites"),
-            'gform':gf,
+            'gform': gf,
         }
         context.update(extra_context or {})
         return super(PenguinSite, self).index(request, context)
+
 
 site = PenguinSite()
 site.register(PenguinUser, PenguinUserAdmin)
@@ -194,4 +202,3 @@ site.register(Camera, CameraAdmin)
 site.register(FlatPage, HelpCMS)
 site.register(FlatPageImage)
 site.register(Revision)
-
