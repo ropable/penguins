709e7f5564ac7a5d3754b9d48ec53cdea9aa52b2
Ashley Felton <ashley@ropable.com>
2016-07-19 10:00:44 +0800

    Revert changes merged from django_1-9 branch.

--- penguins/wsgi.py
+++ penguins/wsgi.py
@@ -1,16 +1,12 @@
 """
 WSGI config for penguins project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "penguins.settings")
-
+import confy
+confy.read_environment_file()  # Must precede dj_static imports.
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
+import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "penguins.settings")
 application = Cling(MediaCling(get_wsgi_application()))
