98c8ebd2b41a133014c0ed912ef8a5258825e72d
Ashley Felton <ashley@ropable.com>
2024-09-05 07:31:17 +0800

    Switch site map to use GDA94 & jpeg base layer.

--- observations/static/js/site_map.js
+++ observations/static/js/site_map.js
@@ -5,15 +5,19 @@
 // layerName
 // penguinIconUrl
 // camerasJson
-const geoserver_wmts_url = geoserverUrl + "/gwc/service/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=mercator&tilematrix=mercator:{z}&tilecol={x}&tilerow={y}&format=image/png";
+const geoserver_wmts_url = geoserverUrl + "/gwc/service/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=gda94&tilematrix=gda94:{z}&tilecol={x}&tilerow={y}&format=image/jpeg";
 
 // Base tile layer
 const baseLayer = L.tileLayer(
   geoserver_wmts_url + "&layer=" + layerName,
+  {
+    tileSize: 1024,
+    zoomOffset: -2,
+  },
 );
 
 // Penguin icon
-var penguinIcon = L.icon({
+const penguinIcon = L.icon({
   iconUrl: penguinIconUrl,
   iconSize: [50, 50],
   iconAnchor: [25, 50],
@@ -21,8 +25,8 @@ var penguinIcon = L.icon({
 });
 
 // Define map and add the base layer
-var map = L.map("map", {
-  crs: L.CRS.EPSG3857,
+const map = L.map("map", {
+  crs: L.CRS.EPSG4326,  // WGS 84
   center: [-32.305, 115.695],
   zoom: 16,
   minZoom: 12,
